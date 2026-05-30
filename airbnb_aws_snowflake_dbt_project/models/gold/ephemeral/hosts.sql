e9473a33aa32a5873409d89feab67c8c593d90f2
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2026-05-30 15:16:57 +0000

    Created SCD Type2 for hosts dim table

---
+++ airbnb_aws_snowflake_dbt_project/models/gold/ephemeral/hosts.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='ephemeral') }}
+
+WITH hosts as(
+    SELECT
+        HOST_ID,
+        HOST_NAME,
+        HOST_SINCE,
+        IS_SUPERHOST,
+        RESPONSE_RATE_QUALITY,
+        HOST_CREATED_AT
+    FROM
+        {{ ref('one_big_table') }}
+)
+SELECT * FROM hosts
